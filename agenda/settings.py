aa69b530a1e3ccb87200758cab4a9fcd14b2a36d
FF-77 <franciscofrancod@gmail.com>
2020-05-04 23:17:53 -0300

    Adicionado return de lista por json, retorno de http404 e configurado o timezone para pt-br

--- agenda/settings.py
+++ agenda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f*!g-66pp3yw6m4t6+pmjhooezqgnx0_pz!(e@n@hr6(zhys#u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
@@ -104,15 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
